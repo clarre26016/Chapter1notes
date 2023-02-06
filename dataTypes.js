de38f45efa517885d69592d8e3f776f62f52d6b6
spbx <spbx@frensou.org>
2023-02-06 13:47:12 -0800

    adding chapter1notes

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+console.log(15);
+
+//This a comment
+
+//numbers
+// Integers
+console.log(-15);
+console.log(0);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//String
+//Characters, letters, symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log(`backticks by the 1`);
+
+//Arithmetic Operations (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                  // it means remainder after dividing
+
+//Concatenation means to combine strings
+console.log("My name "+" is Superman");
+console.log('My'+' '+'age'+' '+'is '+15+'.');
+console.log("Your age is\n16.");
+
+//Template literal - plug in some calculated value
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} disohfpeo`);
---
+++ operations.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+//Binary Operations use two values
+//Comparison Operations
+console.log(10>5); //greater than
+console.log(10<5); //less than
